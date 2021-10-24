368e4dae7315ee00c3ccbac68e644edcb95c25dd
DominikW <dominik.wukovatz@gmail.com>
2021-10-24 22:32:53 +0200

    added applicationinsights

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const appInsights = require("applicationinsights");
+
+appInsights
+  .setup("80228b6a-2129-4ae8-9e40-5578c877a341")
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+  .start();
 
 app.get("/", (req, res) => {
-  res.send("Hallo Welt!");
+  res.send("Hallo Weltii!");
 });
 
 app.listen(port, () => {
